dbad6f214ec5e29b06bc44d0bfedbff1bf2708d2
Agostinho Ourique Cruz <tcruz1971@gmail.com>
2016-03-04 08:44:58 +0100

    Check OS

--- src/com/vogella/jersey/jaxb/model/TodoResource.java
+++ src/com/vogella/jersey/jaxb/model/TodoResource.java
@@ -11,15 +11,21 @@ import java.util.List;
 @Path("/todo")
 public class TodoResource {
   // This method is called if XMLis request
+  private static String OS = System.getProperty("os.name").toLowerCase();
   @GET
   @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
   public Todo getXML() {
     Todo todo = new Todo();
     todo.setSummary("This is my first todo");
     todo.setDescription("This is my first todo");
-	File[] paths;
-	File f = new File("/tmp");
-	paths = f.listFiles();
+	File[] paths = null;
+	if ( OS.indexOf("nux") >= 0 || OS.indexOf("nix") >= 0 || OS.indexOf("aix") > 0 ) {
+		File f = new File("/tmp");
+		paths = f.listFiles();
+	} else if ( OS.indexOf("win") >= 0 ) {
+		File f = new File("d:\\Downloads");
+		paths = f.listFiles();		
+	}
 	List <String> item = new ArrayList<String> ();
 	for (File path:paths) {
 		item.add(path.getName());
